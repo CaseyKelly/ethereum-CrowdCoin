956347750a4899374f7c4fac36eadab02e3dffa4
Casey Kelly <casey.kelly@colorado.edu>
2018-02-01 16:07:09 -0700

    add test script and test that we can deploy factory and campaign

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,10 +1,11 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-const compiledFactory = require("../../ethereum/build/CampaignFactory.json");
-const compiledCampaign = require("../../ethereum/build/Campaign.json");
+const compiledFactory = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
 
 let accounts;
 let factory;
@@ -18,6 +19,8 @@ beforeEach(async () => {
     .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 
+  factory.setProvider(provider);
+
   await factory.methods.createCampaign("100").send({
     from: accounts[0],
     gas: "1000000"
@@ -28,4 +31,11 @@ beforeEach(async () => {
     JSON.parse(compiledCampaign.interface),
     campaignAddress
   );
+});
+
+describe("Campaigns", () => {
+  it("deploys a factory and a campaign", () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
 });
