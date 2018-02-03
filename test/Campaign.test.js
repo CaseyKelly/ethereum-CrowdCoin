674b74d143a28d1f4be7e93736075d752d4e75f8
Casey Kelly <casey.kelly@colorado.edu>
2018-02-03 14:43:33 -0700

    Campaign contract allows a manager to make a payment request

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -46,7 +46,7 @@ describe("Campaigns", () => {
 
   it("allows people to contribute money and marks them as approvers", async () => {
     await campaign.methods.contribute().send({
-      value: '200',
+      value: "200",
       from: accounts[1]
     });
     const isContributor = await campaign.methods.approvers(accounts[1]).call();
@@ -56,11 +56,23 @@ describe("Campaigns", () => {
   it("requires a minimum contribution", async () => {
     try {
       await campaignAddress.methods.contribute().send({
-        value: '5',
+        value: "5",
         from: accounts[1]
       });
     } catch (err) {
       assert(err);
     }
+  });
+
+  it("allows a manager to make a payment request", async () => {
+    await campaign.methods
+      .createRequest("Buy batteries", "100", accounts[1])
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      });
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal("Buy batteries", request.description);
   });
 });
