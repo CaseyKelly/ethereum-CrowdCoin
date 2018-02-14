9c3d05e87eddc0368478f509de20b25f3f7bd761
Casey Kelly <casey.kelly@colorado.edu>
2018-02-13 22:36:01 -0700

    render ContributeForm

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
 
 class ContributeForm extends Component {
-  render() {}
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>Amount to Contribute</label>
+          <Input label="ether" labelPosition="right" />
+        </Form.Field>
+        <Button primary>Contribute</Button>
+      </Form>
+    );
+  }
 }
 
 export default ContributeForm;
